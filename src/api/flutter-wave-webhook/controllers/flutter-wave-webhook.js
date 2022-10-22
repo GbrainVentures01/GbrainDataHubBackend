2a3b9773695c002f12ed9a6eabc2c5280ff51119
layintondev <layintondev@gmail.com>
2022-10-22 22:21:20 +0100

    latest release

--- src/api/flutter-wave-webhook/controllers/flutter-wave-webhook.js
+++ src/api/flutter-wave-webhook/controllers/flutter-wave-webhook.js
@@ -20,93 +20,110 @@ module.exports = createCoreController(
       const reqBody = ctx.request.body;
       console.log(reqBody);
 
-      const user = await strapi
-        .query("plugin::users-permissions.user")
-        .findOne({ where: { email: reqBody.customer.email } });
+      if (reqBody) {
+        const user = await strapi
+          .query("plugin::users-permissions.user")
+          .findOne({ where: { email: reqBody.customer.email } });
 
-      const payload = {
-        data: {
-          status: reqBody.status,
-          amount: Number(reqBody.amount),
-          trx_id: reqBody.id,
-          tx_ref: reqBody.txRef,
-          flw_ref: reqBody.flwRef,
-          currency: reqBody.currency,
-          charged_amount: Number(reqBody.charged_amount),
-          app_fee: Number(reqBody.appfee),
-          //   payment_method: reqBody.data.payment_type,
-          date_time:
-            reqBody.entity.createdAt || new Date(Date.now()).toISOString(),
-          payer_email: reqBody.customer.email,
-        },
-      };
+        const payload = {
+          data: {
+            status: reqBody.status,
+            amount: Number(reqBody.amount),
+            trx_id: reqBody.id,
+            tx_ref: reqBody.txRef,
+            flw_ref: reqBody.flwRef,
+            currency: reqBody.currency,
+            charged_amount: Number(reqBody.charged_amount),
+            app_fee: Number(reqBody.appfee),
+            //   payment_method: reqBody.data.payment_type,
+            date_time:
+              reqBody.entity.createdAt || new Date(Date.now()).toISOString(),
+            payer_email: reqBody.customer.email,
+          },
+        };
 
-      try {
-        await strapi
-          .service("api::flutter-wave-webhook.flutter-wave-webhook")
-          .create(payload);
-        if (reqBody.status === "successful") {
-          console.log("verifying payment...");
-          const flw = new Flutterwave(
-            process.env.FLUTTER_WAVE_PUBLIC_KEY,
-            process.env.FLUTTER_WAVE_TEST_SECRET_KEY
-          );
+        try {
+          await strapi
+            .service("api::flutter-wave-webhook.flutter-wave-webhook")
+            .create(payload);
+          if (reqBody.status === "successful") {
+            console.log("verifying payment...");
+            const flw = new Flutterwave(
+              process.env.FLUTTER_WAVE_PUBLIC_KEY,
+              process.env.FLUTTER_WAVE_LIVE_SECRET_KEY
+            );
 
-          flw.Transaction.verify({ id: reqBody.id })
-            .then(async (response) => {
-              console.log(response);
-              if (
-                response.data.status === "successful" &&
-                response.data.amount === reqBody.amount &&
-                response.data.currency === reqBody.currency
-              ) {
-                await strapi.query("plugin::users-permissions.user").update({
-                  where: { id: user.id },
-                  data: {
-                    AccountBalance:
-                      user.AccountBalance + Number(response.data.amount),
-                  },
-                });
-                await strapi
-                  .query("api::account-funding.account-funding")
-                  .update({
-                    where: { tx_ref: reqBody.txRef },
+            flw.Transaction.verify({ id: reqBody.id })
+              .then(async (response) => {
+                console.log(response);
+                if (
+                  response.data.status === "successful" &&
+                  response.data.amount === reqBody.amount &&
+                  response.data.currency === reqBody.currency
+                ) {
+                  await strapi.query("plugin::users-permissions.user").update({
+                    where: { id: user.id },
                     data: {
-                      status: "Success",
-                      transaction_id: reqBody.id.toString(),
+                      AccountBalance:
+                        user.AccountBalance + Number(response.data.amount),
                     },
                   });
-              } else {
-                const EmailSent = await strapi
-                  .plugin("email")
-                  .service("email")
-                  .send({
-                    to: user.email,
+                  await strapi
+                    .query("api::account-funding.account-funding")
+                    .update({
+                      where: { tx_ref: reqBody.txRef },
+                      data: {
+                        status: "Success",
+                        transaction_id: reqBody.id.toString(),
+                      },
+                    });
+                } else {
+                  const EmailSent = await strapi
+                    .plugin("email")
+                    .service("email")
+                    .send({
+                      to: user.email,
 
-                    subject: "Wallet Funding",
+                      subject: "Wallet Funding",
 
-                    html: `<p>Dear ${user.username}, your payment with the trx ref of ${reqBody.tx_ref} was not successful.</p>
+                      html: `<p>Dear ${user.username}, your payment with the trx ref of ${reqBody.tx_ref} was not successful.</p>
                     <p>Contact Support for more details</p>
                     <p>Gbrain Corporate Ventures.</p>
                     `,
-                  });
-                await strapi
-                  .query("api::account-funding.account-funding")
-                  .update({
-                    where: { tx_ref: reqBody.tx_ref },
-                    data: {
-                      status: "Failed",
-                      transaction_id: reqBody.id,
-                    },
-                  });
-                console.log(EmailSent);
-                // Inform the customer their payment was unsuccessful
-              }
-            })
-            .catch(console.log);
+                    });
+                  await strapi
+                    .query("api::account-funding.account-funding")
+                    .update({
+                      where: { tx_ref: reqBody.tx_ref },
+                      data: {
+                        status: "Failed",
+                        transaction_id: reqBody.id,
+                      },
+                    });
+                  console.log(EmailSent);
+
+                  // Inform the customer their payment was unsuccessful
+                }
+              })
+              .catch((e) => console.log(e));
+          }
+        } catch (error) {
+          console.log(error);
+        } finally {
+          ctx.send(
+            {
+              message: "success",
+            },
+            200
+          );
         }
-      } catch (error) {
-        console.log(error);
+      } else {
+        ctx.send(
+          {
+            message: "bad request",
+          },
+          400
+        );
       }
     },
   })
