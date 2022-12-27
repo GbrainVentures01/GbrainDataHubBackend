b06e0fd7ead64f55fa4be23c3d8ae90437c3d943
layintondev <layintondev@gmail.com>
2022-12-27 20:18:07 +0100

    latest commit

--- src/api/data-house-webhook/controllers/data-house-webhook.js
+++ src/api/data-house-webhook/controllers/data-house-webhook.js
@@ -13,7 +13,24 @@ module.exports = createCoreController(
       const reqBody = ctx.request.body;
       console.log(reqBody);
 
+      const newOrder = {
+        data: {
+          network: reqBody.network,
+          mobile_number: reqBody.mobile_number,
+          status: reqBody.Status,
+          balance_before: reqBody.balance_before,
+          balance_after: reqBody.balance_after,
+          plan_amount: reqBody.plan_amount,
+          ident: reqBody.ident,
+          api_response: reqBody.api_response,
+          refund: reqBody.refund,
+        },
+      };
+
       try {
+        await strapi
+          .service("api::data-house-webhook.data-house-webhook")
+          .create(newOrder);
         if (reqBody.Status === "successful") {
           await strapi.query("api::cg-data-order.cg-data-order").update({
             where: { ident: reqBody.ident },
