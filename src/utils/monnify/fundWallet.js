c6d95f1cf087c41c3e361372eaf6e05b02c490fc
LayintonDev <layintondev@gmail.com>
2023-09-28 18:25:58 +0100

    monnify integration

--- src/utils/monnify/fundWallet.js
+++ src/utils/monnify/fundWallet.js
@@ -37,7 +37,7 @@ module.exports = async ({ userData, amount, ref, gateway }) => {
       currencyCode: "NGN",
       contractCode: process.env.MONNIFY_CONTRACT_CODE,
       redirectUrl: callbackUrl,
-      paymentMethods: ["CARD", "ACCOUNT_TRANSFER"],
+      paymentMethods: ["CARD", "ACCOUNT_TRANSFER", "USSD", "PHONE NUMBER"],
     };
     let monifyToken;
     if (gateway === "monify") {
